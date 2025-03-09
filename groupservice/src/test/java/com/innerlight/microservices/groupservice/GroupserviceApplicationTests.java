84f0e557dfffc6393ca4f59a441b9a5570771041
Facundo Rodriguez <facundo.m.rodriguez1993@gmail.com>
2025-03-09 11:58:40 -0300

    initial commit proyect services

---
+++ groupservice/src/test/java/com/innerlight/microservices/groupservice/GroupserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innerlight.microservices.groupservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
